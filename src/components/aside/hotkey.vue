b7b77cd77731b81317d2d1b7f4075024f147b0bb
刘月松 <liuyuesongde@163.com>
2018-06-06 18:06:19 +0800

    color random

--- src/components/aside/hotkey.vue
+++ src/components/aside/hotkey.vue
@@ -4,23 +4,41 @@
   >
     <div class="tag-list">
       <div class="inner">
-        <a href="#"> 区块链 </a>
-        <a href="#"> 比特币 </a>
-        <a href="#"> 公司 </a>
-        <a href="#"> 火币 </a>
-        <a href="#"> 交易 </a>
-        <a href="#"> 区块链 </a>
-        <a href="#"> 公司 </a>
-        <a href="#"> 火币 </a>
-        <a href="#"> 交易 </a>
-        <a href="#"> 区块链 </a>
+        <a href="#" v-for="(item, index) of list" :key="index" :style="{backgroundColor: colors[parseInt(Math.random() * colors.length)]}">{{item}}</a>
       </div>
     </div>
   </block>
 </template>
 <script>
 export default {
-  
+  data: () => ({
+    colors: [
+      '#009966',
+      '#009999',
+      '#00FF66',
+      '#00FF99',
+      '#339966',
+      '#339999',
+      '#990066',
+      '#990099',
+      '#CC0066',
+      '#CC0099',
+      '#FF6666',
+      '#FF6699',
+    ],
+    list: [
+      '区块链',
+      '比特币',
+      '公司',
+      '火币',
+      '交易',
+      '区块链',
+      '公司',
+      '火币',
+      '交易',
+      '区块链',
+    ]
+  })
 }
 </script>
 <style lang="less" scoped>
