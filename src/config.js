60d22a5942923839ff478acbd54bb8269876126e
xiaohai <ishaiua@163.com>
2018-07-08 19:20:24 +0800

    增加了摘要 去除了 更多

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-//export const APIHOST = 'http://47.104.135.60:8080'
-export const APIHOST = 'http://127.0.0.1:8000'
+export const APIHOST = 'http://47.104.135.60:8080'
+//export const APIHOST = 'http://127.0.0.1:8000'
